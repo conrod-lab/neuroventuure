947800ddc9922e3fdc8c50aef42685cdc5b760d7
Sean Spinney <sean.c.spinney@gmail.com>
2024-01-31 21:25:21 -0500

    Update run_batch_recon_all_long.sh

--- freesurfer/run_batch_recon_all_long.sh
+++ freesurfer/run_batch_recon_all_long.sh
@@ -6,6 +6,9 @@ BIDSROOT=/home/spinney/project/data/neuroventure/bids
 OUTPUT=/scratch/spinney/neuroventure/derivatives/freesurfer
 FREESURFER_DATA=/scratch/spinney/neuroventure/derivatives/freesurfer/sourcedata
 
+# copy data from root to freesurfer directory
+rsync -rv $BIDSROOT/* $FREESURFER_DATA/
+
 # find all DICOM directories that start with "voice"
 subject_numbers=($(find "$FREESURFER_DATA" -maxdepth 2 -type d -name "sub-*" | cut -d'-' -f2))
 subject_numbers=(155)
