99300d463ad18c5d7aca3e069db395c9bba4c7e3
scspinney <spinney@cedar1.cedar.computecanada.ca>
2024-01-31 18:39:40 -0800

    Minor improvements

--- freesurfer/run_batch_recon_all_long.sh
+++ freesurfer/run_batch_recon_all_long.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-#set -eu
+BIDSROOT="/home/spinney/project/data/neuroventure/bids"
+OUTPUT="/scratch/spinney/neuroventure/derivatives/freesurfer"
+FREESURFER_DATA="${OUTPUT}/sourcedata"
+LOGDIR="${SCRATCH}/freesurfer-logs"
 
-BIDSROOT=/home/spinney/project/data/neuroventure/bids
-OUTPUT=/scratch/spinney/neuroventure/derivatives/freesurfer
-FREESURFER_DATA=/scratch/spinney/neuroventure/derivatives/freesurfer/sourcedata
+mkdir -p "${FREESURFER_DATA}"
+mkdir -p "${LOGDIR}"
 
-# find all DICOM directories that start with "voice"
-subject_numbers=($(find "$FREESURFER_DATA" -maxdepth 2 -type d -name "sub-*" | cut -d'-' -f2))
+
+# Copy data from root to FreeSurfer directory
+rsync -rv "${BIDSROOT}"/* "${FREESURFER_DATA}/"
+
+# Find all DICOM directories that start with "sub"
+subject_numbers=($(find "${FREESURFER_DATA}" -maxdepth 2 -type d -name "sub-*" | cut -d'-' -f2))
 subject_numbers=(155)
 
-
-#More ressources
+# More resources
 sbatch --array=0-`expr ${#subject_numbers[@]} - 1`%100 \
        --cpus-per-task=8 \
        --mem=10GB \
        --time=12:00:00 \
-       --output=/home/spinney/scratch/neuroventure/raw/tmp/output/freesurfer/recon-all-long_%A_%a.out \
-       --error=/home/spinney/scratch/neuroventure/raw/tmp/error/freesurfer/recon-all-long_%A_%a.err \
-       run_recon_all_long.sh ${FREESURFER_DATA} ${OUTPUT} ${subject_numbers[@]}
+       --output="${LOGDIR}/recon-all-long_%A_%a.out" \
+       --error="${LOGDIR}/recon-all-long_%A_%a.err" \
+       run_recon_all_long.sh "${FREESURFER_DATA}" "${OUTPUT}" "${subject_numbers[@]}"
